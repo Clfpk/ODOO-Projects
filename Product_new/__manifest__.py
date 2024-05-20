2b3d6bc92ef0c83dee1b144800fee7d11fc0a81a
Clfpk <142084167+Clfpk@users.noreply.github.com>
2024-05-20 10:58:47 +0530

    Add files via upload

---
+++ Product_new/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product_New',
+    'author': 'Cloud Infosoft',
+    'version': '17.0',
+    'license': 'LGPL-3',
+    'sequence': 1,
+    'depends': ['base','sale'],
+    'data': [ ],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
